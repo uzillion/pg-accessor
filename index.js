4d99b0eb5504de2d12ba2df957b0648ae46ac501
uzillion <uzair_inamdar@hotmail.com>
2018-07-08 16:33:10 -0700

    Complete generation of getter files

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+#!/usr/bin/env node
 // if(process.env.NODE_ENV == "development") {
   require('dotenv').config();
 // }
+const path = require('path');
 const getTables = require('./lib/db/getTables');
 const genConfig = require('./lib/build_config');
+const argv = require('yargs').argv;
+const genFiles = require('./lib/genFiles');
 
-getTables().then((tables) => {
-  genConfig(tables);
-});
+switch(argv._[0]) {
+  case "init":
+    getTables().then((tables) => {
+      genConfig(tables);
+    });
+    break;
+  case "build":
+    // console.log(path.resolve('./db'));
+    const build_obj = require(path.resolve('./db/config.js'));
+    genFiles(build_obj);
+    break;
+
+}
+
+
