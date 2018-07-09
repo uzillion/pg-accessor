4917a84392d8fcfd1545d4674538828df205deb8
uzillion <uzair_inamdar@hotmail.com>
2018-07-08 23:49:29 -0700

    Handle missing ./db directory
    Display proper message if "build" is tried to run without "init"

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const path = require('path');
 const getTables = require('./lib/db/getTables');
 const init = require('./lib/init');
 const build = require('./lib/build');
+const fs = require('fs');
 
 const argv = require('yargs')
 .command({
   command: 'init',
   desc: 'Initialize the build process by creating config.js in db/',
   handler: (argv) => {
+    if(!fs.existsSync(path.resolve('./db'))) {
+      fs.mkdirSync(path.resolve('./db'));
+    }
     getTables().then((tables) => {
       init(tables);
     });
@@ -20,8 +24,13 @@ const argv = require('yargs')
   command: 'build',
   desc: 'Build the getter and setter functions in db/',
   handler: (argv) => {
-    const build_obj = require(path.resolve('./db/config.js'));
-    build(build_obj);
+    if(fs.existsSync(path.resolve('./db/config.js'))) {
+      const build_obj = require(path.resolve('./db/config.js'));
+      build(build_obj);
+    } else {
+      console.log('You need to run "accessor init" before running build command');
+      process.exit(1);
+    }
   }
 }).demandCommand(1, "You need mention either 'init' or 'build'")
 .help()
